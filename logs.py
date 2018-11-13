0115b254487a05c71e3a6398943c5bb8dfdda315
Trentham3269 <scottdalwood@gmail.com>
2018-11-13 09:52:59 +0000

    Add global db connection handler

--- logs.py
+++ logs.py
@@ -5,6 +5,12 @@ import psycopg2
 # Connection string
 dbname = "news"
 
+# Connect to database
+try:
+    db = psycopg2.connect(database=dbname)
+except:
+    print("Unable to connect to the database")
+
 
 # Three most popular articles of all time
 def get_articles():
@@ -12,11 +18,9 @@ def get_articles():
         select title, views
         from views fetch first 3 rows only;
         '''
-    db = psycopg2.connect(database=dbname)
     c = db.cursor()
     c.execute(sql)
     articles = c.fetchall()
-    db.close()
     print("The three most popular articles...")
     for a in articles:
         print('Title: {0}, Article views: {1}'.format(a[0], a[1]))
@@ -33,11 +37,9 @@ def get_authors():
         group by name
         order by total desc;
         '''
-    db = psycopg2.connect(database=dbname)
     c = db.cursor()
     c.execute(sql)
     authors = c.fetchall()
-    db.close()
     print("The most popular authors...")
     for a in authors:
         print('Author: {0}, Total views: {1}'.format(a[0], a[1]))
@@ -53,14 +55,16 @@ def get_errors():
         from error_rates
         where error_rate > 1
         '''
-    db = psycopg2.connect(database=dbname)
     c = db.cursor()
     c.execute(sql)
     errors = c.fetchall()
-    db.close()
     print("Big error days...")
     for e in errors:
         print('Date: {0}, Error rate: {1}%'.format(e[0], e[1]))
 
 
 get_errors()
+
+
+# Disconnect from database
+db.close()
