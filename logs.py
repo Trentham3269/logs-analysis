dd583b7c099d1c8bc98db9c451714250df279ce7
Trentham3269 <scottdalwood@gmail.com>
2018-11-13 09:35:06 +0000

    Wrap sql queries to reduce characters per line

--- logs.py
+++ logs.py
@@ -8,7 +8,10 @@ dbname = "news"
 
 # Three most popular articles of all time
 def get_articles():
-    sql = "select title, views from views fetch first 3 rows only;"
+    sql = '''
+        select title, views
+        from views fetch first 3 rows only;
+        '''
     db = psycopg2.connect(database=dbname)
     c = db.cursor()
     c.execute(sql)
@@ -18,11 +21,18 @@ def get_articles():
     for a in articles:
         print('Title: {0}, Article views: {1}'.format(a[0], a[1]))
 
+
 get_articles()
+
 
 # Most popular authors of all time
 def get_authors():
-    sql = "select name, sum(views) as total from views group by name order by total desc;"
+    sql = '''
+        select name, sum(views) as total
+        from views
+        group by name
+        order by total desc;
+        '''
     db = psycopg2.connect(database=dbname)
     c = db.cursor()
     c.execute(sql)
@@ -32,11 +42,17 @@ def get_authors():
     for a in authors:
         print('Author: {0}, Total views: {1}'.format(a[0], a[1]))
 
+
 get_authors()
+
 
 # Days in which more than 1% of requests were errors
 def get_errors():
-    sql = "select date(day), round(error_rate,2) from error_rates where error_rate > 1"
+    sql = '''
+        select date(day), round(error_rate,2)
+        from error_rates
+        where error_rate > 1
+        '''
     db = psycopg2.connect(database=dbname)
     c = db.cursor()
     c.execute(sql)
@@ -45,5 +61,6 @@ def get_errors():
     print("Big error days...")
     for e in errors:
         print('Date: {0}, Error rate: {1}%'.format(e[0], e[1]))
+
 
 get_errors()
